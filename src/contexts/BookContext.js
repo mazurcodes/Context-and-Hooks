b13c01e8d25f32923d051ba84b61f0f8b0f3f446
Bartłomiej Mazur <ab.mazur@gmail.com>
2019-07-28 10:19:17 +0200

    Local storage and use Reducer lazy initialization

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,18 +1,30 @@
-
-import React, { createContext, useReducer } from 'react';
-import { bookReducer } from '../reducers/BookReducer';
+import React, { createContext, useReducer, useEffect } from "react";
+import { bookReducer } from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
-const BookContextProvider = (props) => {
+  // INITIAL VALUE FOR REDUCER lazy initial
+  // check if there are books in local storage... if not then return empty array
+const localBooks = () => {
+  return localStorage.getItem("books")
+    ? JSON.parse(localStorage.getItem("books"))
+    : [];
+};
 
-  const [books, dispatch] = useReducer(bookReducer, [])
+const BookContextProvider = props => {
+
+  const [books, dispatch] = useReducer(bookReducer, [], localBooks);
+
+  // update local storage only if {books} has changed...
+  useEffect(() => localStorage.setItem("books", JSON.stringify(books)), [
+    books
+  ]);
 
   return (
-    <BookContext.Provider value={{books, dispatch}}>
-      {props.children}     
+    <BookContext.Provider value={{ books, dispatch }}>
+      {props.children}
     </BookContext.Provider>
-  )
-}
+  );
+};
 
-export default BookContextProvider
+export default BookContextProvider;
